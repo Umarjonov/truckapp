779886885588607ade3dfb02e4cef13360f82161
takhirovulugbek777 <129583619+takhirovulugbek777@users.noreply.github.com>
2023-09-11 11:19:42 +0500

    Added details

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = \App\Models\User::paginate(10); // 10 users per page
+
+        return view('users.index', ['users' => $users]);
+    }
+
+    public function admin()
+    {
+        return view('admin.admin');
+    }
+
+    public function list()
+    {
+        return view('admin.userlist');
+    }
+}
