76bf525dcb3f401c2ccc225f75644c6e5fd3a976
Ulugbekt566@gmail.com <Ulugbek@quramizuz>
2023-09-29 11:19:07 +0500

    exception api response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -43,8 +46,60 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (\Exception $e, $request) {
+
+            if ($request->is('api/*')) {
+                $id = $request->id??1;
+
+                // Route not found
+                if ($e instanceof NotFoundHttpException) {
+                    return self::error(-32600, $id ?? "");
+                } // Request method not allowed for this route
+                elseif ($e instanceof MethodNotAllowedHttpException) {
+                    return self::error(-32601, $id ?? "");
+                } // No entry was found for this identifier.
+                elseif ($e instanceof NotFoundExceptionInterface) {
+                    return self::error(-32700, $id ?? "");
+                } else {
+                    return self::wrapper(500, $e->getMessage() . ". Line:" . $e->getLine() . ". File:" . pathinfo($e->getFile())['basename']);
+                }
+            }
+
+            if ($e instanceof NotFoundHttpException) {
+                return redirect('/');
+            }
+            if (auth()->guest()) {
+                return redirect()->route('login');
+            }
+            return parent::render($request, $e);
         });
     }
+    public static function error($code, $id = null)
+    {
+//        $error = Error::where('code', $code)->first();
+//        if ($error !=null)
+//            return self::wrapper($error->code, $error->en);
+//        else
+        return self::wrapper($code, "Undefined error");
+    }
+
+    private static function wrapper($code, $message, $id =null){
+
+        $message = [
+            "jsonrpc"   => "2.0",
+            "status"    => false,
+            "origin"    => "any.error",
+            "error"     => [
+                "code"      => $code,
+                "message"   => $message ?? "Undefined Error"
+            ],
+            'host'      => [
+                'name'      => config('app.name'),
+                'time_stamp'=> date('Y-m-d H:i:s', time())
+            ]
+        ];
+
+        return response()->json($message);
+    }
+
 }
